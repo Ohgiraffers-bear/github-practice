d513166b08691c9279556ab3354ecb9aab7cada9
bear-Ohgiraffers <taegeun0525@gmail.com>
2024-02-16 09:11:36 +0900

    first commit

---
+++ src/main/java/org/ohgiraffers/githubpractice/GithubPracticeApplication.java
@@ -0,0 +1,14 @@
+package org.ohgiraffers.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GithubPracticeApplication.class, args);
+    }
+
+}
